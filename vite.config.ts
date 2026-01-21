fdefd4f532dccdaf32f9b232d1480119fc166353
Huub <115623884+HuubHamstra@users.noreply.github.com>
2026-01-21 14:00:50 +0100

    fix: update base path in Vite configuration to match project name

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/delayed-alarm-simulator/",
+  base: "/cyberdrill-engine-room-demo/",
   build: {
     outDir: "docs",
   },
